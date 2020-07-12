5b4f11da9f4e556ef7bd34809807933b8b03642a
kryderius <krydegamer@gmail.com>
2020-07-12 15:03:51 +0200

    My first repo

---
+++ main.js
@@ -0,0 +1,282 @@
+const hamburger = document.querySelector(".hamburger");
+const navMobile = document.querySelector(".nav-mobile");
+
+const handleClick = () => {
+  hamburger.classList.toggle("hamburger--active");
+  navMobile.classList.toggle("nav-mobile--active");
+};
+
+hamburger.addEventListener("click", handleClick);
+
+/* Animations in Section HERO */
+
+var heroH1 = new TimelineMax();
+var heroSvg = new TimelineMax();
+var heroSvg2 = new TimelineMax();
+var nav = new TimelineMax();
+var aboutUs = new TimelineMax();
+var parallaxProduct = new TimelineMax();
+var parallaxProduct2 = new TimelineMax();
+var logos = new TimelineMax();
+var productText = new TimelineMax();
+var productText2 = new TimelineMax();
+var parallaxMountain = new TimelineMax();
+var featureBox = new TimelineMax();
+var videoImg = new TimelineMax();
+var videoScale = new TimelineMax();
+var contactSpan = new TimelineMax();
+var contactAnim = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+heroH1
+  .from("#hero-h1", 1, {
+    opacity: 0,
+    x: "-20px",
+    ease: Power3.easeInOut,
+  })
+  .from(
+    ".hero-title p",
+    1,
+    {
+      opacity: 0,
+      y: "20px",
+      ease: Power3.easeInOut,
+    },
+    "-=0.5"
+  );
+heroSvg.to("#camper", 20, {
+  x: "-100%",
+});
+heroSvg2.from("#camper", 1, {
+  x: "100%",
+  ease: Power4.easeInOut,
+});
+
+nav
+  .to("nav", 1, {
+    padding: "0% 3%",
+    height: "100px",
+    ease: Back.easeInOut,
+  })
+  .to(".logo", 0.2, { opacity: 1 })
+  .to(".hamburger", 0.2, { opacity: 1 }, "-=0.2");
+
+aboutUs
+  .from("#about-us-p", 1, {
+    opacity: 0,
+    x: "-30px",
+  })
+  .from("#about-us-p span", 0.5, {
+    opacity: 0,
+  })
+  .from("#about-us-img", 1, {
+    opacity: 0,
+    y: "-30px",
+  });
+
+parallaxProduct.fromTo(
+  "#mobiletoilet-img",
+  1,
+  { y: "20%", rotation: 0, ease: Linear.easeNone },
+  { y: "-20%", rotation: "8", ease: Linear.easeNone }
+);
+parallaxProduct2.fromTo(
+  "#odor-stop-img",
+  1,
+  { y: "20%", rotation: 0, ease: Linear.easeNone },
+  { y: "-20%", rotation: "-8", ease: Linear.easeNone }
+);
+
+logos.from(".logos-wrapper", 1, { x: "-50%" });
+
+productText.fromTo(
+  ".mobiletoilet-text",
+  1,
+  { y: "-20px", ease: Linear.easeNone },
+  { y: "20px", ease: Linear.easeNone }
+);
+productText2.fromTo(
+  ".odor-stop-text",
+  1,
+  { y: "-20px", ease: Linear.easeNone },
+  { y: "20px", ease: Linear.easeNone }
+);
+parallaxMountain
+  .from("#mountain1", 1, { y: "20%" })
+  .from("#mountain2", 1, { y: "10%" }, "-=1");
+
+featureBox
+  .from("#feature-span", 1, { y: "-100%", ease: Power4.easeInOut })
+  .from(
+    ".feature2 #feature-span",
+    1,
+    { y: "-100%", ease: Power4.easeInOut },
+    "-=0.8"
+  )
+  .from(
+    ".feature3 #feature-span",
+    1,
+    { y: "-100%", ease: Power4.easeInOut },
+    "-=0.8"
+  )
+  .from(
+    ".feature4 #feature-span",
+    1,
+    { y: "-100%", ease: Power4.easeInOut },
+    "-=0.8"
+  );
+
+videoImg.fromTo("#video-img", 1, { y: "30%" }, { y: "-10%" });
+videoScale.from("#video", 1, { scale: "0.9", ease: Back.easeInOut });
+
+contactSpan
+  .from("#contact-span1", 1, { x: "-30%" })
+  .from("#contact-span2", 1, { x: "50%" }, "-=1");
+
+contactAnim
+  .from("#contact-h3", 0.8, { opacity: 0, y: "20px", ease: Back.easeInOut })
+  .from(
+    ".contact-text-wrapper",
+    0.8,
+    { opacity: 0, x: "-20px", ease: Back.easeInOut },
+    "-=0.5"
+  )
+  .from(
+    ".googlemap-section",
+    0.8,
+    {
+      opacity: 0,
+      x: "30px",
+      ease: Back.easeInOut,
+    },
+    "-=0.5"
+  );
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: "onEnter",
+})
+  .setTween(heroH1)
+  .addTo(controller);
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "nav",
+  triggerHook: 0,
+  duration: "100%",
+})
+  .setTween(heroSvg)
+  .addTo(controller);
+
+const scene3 = new ScrollMagic.Scene({
+  triggerElement: ".hero-title",
+  triggerHook: 0,
+})
+  .setTween(nav)
+  .addTo(controller);
+
+const scene4 = new ScrollMagic.Scene({
+  triggerElement: ".about-us",
+  triggerHook: 0.75,
+})
+  .setTween(aboutUs)
+  .addTo(controller);
+
+const scene5 = new ScrollMagic.Scene({
+  triggerElement: ".product-section",
+  triggerHook: 0.5,
+  duration: "100%",
+})
+  .setTween(parallaxProduct)
+  .addTo(controller);
+
+const scene6 = new ScrollMagic.Scene({
+  triggerElement: ".odor-stop",
+  triggerHook: 0.5,
+  duration: "100%",
+})
+  .setTween(parallaxProduct2)
+  .addTo(controller);
+
+const scene7 = new ScrollMagic.Scene({
+  triggerElement: ".about-us",
+  triggerHook: 0.25,
+  duration: "70%",
+})
+  .setTween(logos)
+  .addTo(controller);
+
+const scene8 = new ScrollMagic.Scene({
+  triggerElement: ".product-section",
+  triggerHook: 0.5,
+  duration: "90%",
+})
+  .setTween(productText)
+  .addTo(controller);
+
+const scene9 = new ScrollMagic.Scene({
+  triggerElement: ".odor-stop",
+  triggerHook: 0.5,
+  duration: "90%",
+})
+  .setTween(productText2)
+  .addTo(controller);
+
+const scene10 = new ScrollMagic.Scene({
+  triggerElement: ".parallax-mountains",
+  triggerHook: 0.5,
+  duration: "100%",
+})
+  .setTween(parallaxMountain)
+  .addTo(controller);
+
+const scene11 = new ScrollMagic.Scene({
+  triggerElement: ".features-section",
+  triggerHook: 0.5,
+})
+  .setTween(featureBox)
+  .addTo(controller);
+
+const scene12 = new ScrollMagic.Scene({
+  triggerElement: ".agachem-video",
+  triggerHook: 0.5,
+  duration: "100%",
+})
+  .setTween(videoImg)
+  .addTo(controller);
+
+const scene13 = new ScrollMagic.Scene({
+  triggerElement: ".agachem-video",
+  triggerHook: 0.5,
+})
+  .setTween(videoScale)
+  .addTo(controller);
+
+const scene14 = new ScrollMagic.Scene({
+  triggerElement: ".contact-section",
+  triggerHook: 0.5,
+  duration: "100%",
+})
+  .setTween(contactSpan)
+  .addTo(controller);
+
+const scene15 = new ScrollMagic.Scene({
+  triggerElement: ".contact-text",
+  triggerHook: 0.5,
+})
+  .setTween(contactAnim)
+  .addTo(controller);
+
+var pinNav = new ScrollMagic.Scene({
+  triggerElement: "nav",
+  triggerHook: 0,
+})
+  .setPin("nav", { pushFollowers: false })
+  .addTo(controller);
+/*
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + e.pageY + "px; left: " + e.pageX + "px;"
+  );
+});
+*/
